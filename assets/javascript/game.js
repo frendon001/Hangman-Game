26cb91079182e33fc7a85daf58e0fd7816d15285
Fausto Rendon <faustor001@gmail.com>
2017-08-27 13:23:27 -0700

    first draft of hangman game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,13 @@
-var test = [];
-console.log(test);
-console.log(test.length);
-//Declare and initialize variables for game session in hangman object
-
+//declare and initialize global variables for game
+var displayCurrentWord = document.getElementById("current-word");
+var displayWinCount = document.getElementById("win-count");
+var displayRemainingGuesses = document.getElementById("number-guesses");
+var displayLettersGuessed = document.getElementById("letters-guessed");
+var isCapLetterReg = /[A-Z]/g;
+var playerGuess = "";
+// create hangman object for the game
 var hangman = {
+	//Declare and initialize variables for game session in hangman object
 	wins: 0,
 	loses: 0,
 	remainingGuesses: 12,
@@ -11,6 +15,7 @@ var hangman = {
 	wordsUsed: [],
 	currentWord: "",
 	currentWordState: "",
+	currentWordArr: [],
 	//create array of possible guess options
 	wordList: ["Constellation", "Meteorite", "Eclipse", "Micrometeorites"],
 	newWord: function() {
@@ -24,41 +29,86 @@ var hangman = {
 	},
 	wordPlaceHolder: function() {
 		// var wordEdit = this.currentWord;
-		var regex = /[a-z]|[A-Z]/g;
-		this.currentWordState = this.currentWord.replace(regex, "_");
+		this.currentWordState = this.currentWord.replace(isCapLetterReg, "_");
+		isCapLetterReg.lastIndex = 0;
 	}
 
 };
 
-var displayCurrentWord = document.getElementById("current-word");
+
 
 //assign first word to guess for the game
 hangman.newWord();
+
 console.log(hangman.currentWord);
 console.log(hangman.wordsUsed);
 
 //populate current word on page with placeholders
 hangman.wordPlaceHolder();
 displayCurrentWord.textContent = hangman.currentWordState;
+displayRemainingGuesses.textContent = hangman.remainingGuesses;
 
+hangman.currentWordArr = hangman.currentWord.split("");
+
+console.log(hangman.currentWordArr);
+
+var newWordState = "";
 
 //Start/continue game on player key input
 document.onkeyup = function(event) {
 
 	// Determines which key was pressed.
-	var playerGuess = event.key.toUpperCase();
-	console.log(playerGuess);
+	playerGuess = event.key.toUpperCase();
 
-	//compare the playerGuess against current value and determine if guess is correct
+	//need to either re-initialize the regex or reset the last index for regex to 0
+	//for the .test() method to work properly otherwise there is a strange alternating evaluation of the expression
+	//https://siderite.blogspot.com/2011/11/careful-when-reusing-javascript-regexp.html
+	// isCapLetterReg = /[A-Z]/g;
+	isCapLetterReg.lastIndex = 0;
+	// var isValidKey = isCapLetterReg.test(playerGuess);
+	
 
-	//update the letters guessed array and display changes on webpage
+	//Validadate that a A-Z letterkey was entered otherwise do nothing
+	//Also validate that current guess is new
+	if (isCapLetterReg.test(playerGuess) && playerGuess.length === 1 && hangman.lettersGuessed.indexOf(playerGuess) === -1) {
+		console.log("Valid Key Entered: " + playerGuess);
+		
 
-	//check if word if word guess is complete
+		//update letters guessed array with new guess
+		hangman.lettersGuessed.push(playerGuess);
+		displayLettersGuessed.textContent = hangman.lettersGuessed.join(",");
 
-	//check if player ran out of guesses
+
+		//compare the playerGuess against current value and determine if guess is correct
+		if (hangman.currentWordArr.indexOf(playerGuess) > -1 ) {
+			console.log("Correct: " + hangman.currentWordArr.indexOf(playerGuess));
+			//Guess is correct, update currentWordState and display changes on webpage
+			newWordState = hangman.currentWordState.split("");
+			console.log(newWordState);
+			for (var i = newWordState.length - 1; i >= 0; i--) {
+				if (hangman.currentWordArr[i] === playerGuess) {
+					newWordState[i] = hangman.currentWordArr[i];
+				}
+			}
+
+			hangman.currentWordState = newWordState.join("");
+			console.log(hangman.currentWordState);
+			displayCurrentWord.textContent = hangman.currentWordState;
 
 
+		} else {
+			//incorrect guess
+			//decrease numberof guesses since valid key was pressed 
+			hangman.remainingGuesses -= 1;
+			displayRemainingGuesses.textContent = hangman.remainingGuesses;
+		}
 
+		//update the letters guessed array and display changes on webpage
+
+		//check if word guess is complete
+
+		//check if player ran out of guesses
+	}
 
 
 
